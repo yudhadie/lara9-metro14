5c3406e7a05413a3dc9eef36bbba42212ab25714
yudhadie <yudha.tp3@gmail.com>
2022-07-15 09:57:57 +0700

    repair-modal

---
+++ resources/views/admin/templates/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="footer py-4 d-flex flex-column flex-md-row flex-stack" id="kt_footer">
+    <div class="text-dark order-2 order-md-1">
+        <span class="text-muted fw-bold me-1">2022©</span>
+        <a href="https://www.jjpromotion.co.id" target="_blank" class="text-gray-800 text-hover-primary">JJPromotion</a>
+    </div>
+    <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+        <li class="menu-item">
+            <a href="" target="_blank" class="menu-link px-2">About</a>
+        </li>
+        <li class="menu-item">
+            <a href="" target="_blank" class="menu-link px-2">Support</a>
+        </li>
+        <li class="menu-item">
+            <a href="" target="_blank" class="menu-link px-2">Purchase</a>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/admin/templates/partials/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <title>JJPromotion</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="JJPromotion" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+
+    @yield('styles')
+</head>
+
