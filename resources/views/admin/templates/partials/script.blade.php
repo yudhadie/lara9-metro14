b8f90106472e19b8155624a5fdc05940da1c4ca7
yudhadie <yudha.tp3@gmail.com>
2022-07-16 08:58:13 +0700

    delete

--- resources/views/admin/templates/partials/script.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
-<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
-<script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
-
-
-@stack('scripts')
--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="/">
-    <img src="{{ asset('assets/logo/logo-min.png') }}" class="navbar-brand-img" alt="...">
-</a>
